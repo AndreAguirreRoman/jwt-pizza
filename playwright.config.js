37082aacde3d2f0c52d70632b92641323fc06556
AndreAguirreRoman <ankar_aguirre@live.com>
2026-02-10 18:09:38 -0700

    Had to reinstall since it was detecting TS insted of JS

---
+++ playwright.config.js
@@ -0,0 +1,23 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  reporter: 'html',
+  timeout: 5000,
+  use: {
+    baseURL: 'http://localhost:5173',
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'], viewport: { width: 800, height: 600 } },
+    },
+  ],
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:5173',
+    reuseExistingServer: !process.env.CI,
+    timeout: 5000,
+  },
+});
